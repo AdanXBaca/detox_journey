a60e6c1f060650b6a5a13dfa647bb89193660c9f
Adan Baca <adan@baca.us>
2023-07-12 18:23:37 -0400

    Fixed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.title("Patient Addiction Journey Models")
 tab_SEIHR, tab_SEIHTR = st.tabs(["SEIHR", "SEIHTR"])
 
 with tab_SEIHR:
-    st.text_area('''The following model represents the jouney of addiction
+    st.text('''The following model represents the jouney of addiction
     patients, as a population. Each compartment represents a state in the
     journey. S represents susceptible, which could be anyone. E is an 
     exposed state, and I is infected. H represents hospitalization,
